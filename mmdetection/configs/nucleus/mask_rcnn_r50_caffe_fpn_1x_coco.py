8be646724aa87954205237e242d1b4318c198225
joycenerd <joycenerd.cs09@nycu.edu.tw>
2021-12-01 21:45:56 +0800

    add config

--- mmdetection/configs/nucleus/mask_rcnn_r50_caffe_fpn_1x_coco.py
+++ mmdetection/configs/nucleus/mask_rcnn_r50_caffe_fpn_1x_coco.py
@@ -1,5 +1,5 @@
 # The new config inherits a base config to highlight the necessary modification
-_base_ = '../mask_rcnn/mask_rcnn_r50_fpn_mstrain-poly_3x_coco.py'
+_base_ = '../mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
 
 # We also need to change the num_classes in head to match the dataset's annotation
 model = dict(
@@ -11,10 +11,12 @@ model = dict(
 dataset_type = 'COCODataset'
 classes = ('nucleus',)
 data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=1,
     train=dict(
-        img_prefix='/eva_data/zchin/nucleus_data/train',
+        img_prefix='/eva_data/zchin/nucleus_data/train/',
         classes=classes,
-        ann_file='/eva_data/zchin/nucleus_data/annotations/instance_train.json'),
+        ann_file='/eva_data/zchin/nucleus_data/annotations/instance_train_copy.json'),
     val=dict(
         img_prefix='/eva_data/zchin/nucleus_data/val',
         classes=classes,
@@ -22,8 +24,6 @@ data = dict(
     test=dict(
         img_prefix='/eva_data/zchin/nucleus_data/val',
         classes=classes,
-        ann_file='/eva_data/zchin/nucleus_data/annotations/instance_val.json')
-    )
-
-# We can use the pre-trained Mask RCNN model to obtain higher performance
-load_from = '/home/zchin/mmdet-nucleus-instance-segmentation/mmdetection/checkpoints/mask_rcnn_r50_caffe_fpn_1x_coco_bbox_mAP-0.38__segm_mAP-0.344_20200504_231812-0ebd1859.pth'
+        ann_file='/eva_data/zchin/nucleus_data/annotations/instance_val.json'))
+    
+load_from ='/home/zchin/mmdet-nucleus-instance-segmentation/mmdetection/checkpoints/mask_rcnn_r50_caffe_fpn_1x_coco_bbox_mAP-0.38__segm_mAP-0.344_20200504_231812-0ebd1859.pth'
---
+++ mmdetection/configs/nucleus/mask_rcnn_r50_fpn_mstrain-poly_3x_nucleus.py
@@ -0,0 +1,69 @@
+_base_ = '../mask_rcnn/mask_rcnn_r50_fpn_mstrain-poly_3x_coco.py'
+
+# dataset settings
+dataset_type = 'CocoDataset'
+classes = ('nucleus',)
+data_root = '/eva_data/zchin/nucleus_data/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+# In mstrain 3x config, img_scale=[(1333, 640), (1333, 800)],
+# multiscale_mode='range'
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='LoadAnnotations',
+        with_bbox=True,
+        with_mask=True,
+        poly2mask=False),
+    dict(
+        type='Resize',
+        img_scale=[(1333, 640), (1333, 800)],
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+# Use RepeatDataset to speed up training
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        dataset=dict(
+            type=dataset_type,
+            classes=classes,
+            ann_file=data_root + 'annotations/instance_train.json',
+            img_prefix=data_root + 'train/',
+            pipeline=train_pipeline)),
+    val=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'annotations/instance_val.json',
+        img_prefix=data_root + 'val',
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        classes=classes,
+        ann_file=data_root + 'annotations/instance_val.json',
+        img_prefix=data_root + 'val',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric=['bbox', 'segm'])
